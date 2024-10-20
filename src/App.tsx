a6e1c43652c6549d013f55639aeffc5dc876aea2
Alex Pitenko <oleksii.p@itsyndicate.org>
2024-10-20 20:31:37 +0200

    added getting user firstName

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
+import { retrieveLaunchParams } from '@telegram-apps/sdk-react';
+
 
 function App() {
+  const launchParams = retrieveLaunchParams();
   return (
     <>
       <header>
         <h1>MentalFit</h1>
       </header>
+      <h2>Hello, {launchParams.initData?.user?.firstName}</h2>
     </>
   )
 }
