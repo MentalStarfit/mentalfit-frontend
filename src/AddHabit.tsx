7bf8b0150e61a490a70ce4929bbe52123c767a3a
Kastet <ostap99.2002@gmail.com>
2024-11-09 04:15:34 +0200

    AddHabit layout microfix

--- src/AddHabit.tsx
+++ src/AddHabit.tsx
@@ -1,16 +1,26 @@
-import './Home.css';
-
+import './AddHabit.css';
 
 function AddHabit(){
 
-return (
-    <>
-    <div className={"add-habit-popup"}>
-        <p>AAAAA</p>
-    </div>
-</>
+    return (
+        <>
+            <div className={"create-habit-popup"}>
+                <button className={"create-habit-button"}>+</button>
+                <input type="text" className={"habit-input"}/>
+                <p>repeat every</p>
+                <div className={"repeat-row"}>
+                    <input type={"number"} className={"input-number"} min="1"/>
+                    <select name="interval" id="interval">
+                        <option value="day">day</option>
+                        <option value="week">week</option>
+                        <option value="month">month</option>
+                    </select>
+                </div>
+                <input type="submit" className={"create-habbit-button"}/>
+            </div>
+        </>
 
-)
+    )
 }
 
 export default AddHabit
